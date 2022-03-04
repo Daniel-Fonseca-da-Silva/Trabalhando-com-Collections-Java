7f1dcfa9c6369e63aba5b7b59412b650140334e8
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-03-04 13:53:57 -0300

    Conhecendo os métodos List - parte 1

--- src/com/dio/list/ExemploList.java
+++ src/com/dio/list/ExemploList.java
@@ -17,17 +17,41 @@ class ExemploList {
         notas.remove(5d);
         System.out.println(notas);*/
 
-//        System.out.println("Crie uma lista e adicione as sete notas: ");
-//
-//        System.out.println("Exiba a posição da nota 5.0: ");
-//
-//
-//        System.out.println("Substitua a nota 5.0 pela nota 6.0: ");
-//
-//        System.out.println("Confira se a nota 5.0 está na lista: ");
-//
-//        System.out.println("Exiba todas as notas na ordem em que foram informados: ");
-//
+        System.out.println("Crie uma lista e adicione as sete notas: ");
+
+        List<Double> notas = new ArrayList<>();
+
+        notas.add(7D);
+        notas.add(8.5);
+        notas.add(9.3);
+        notas.add(5D);
+        notas.add(7D);
+        notas.add(0D);
+        notas.add(3.6);
+
+        System.out.println(notas.toString());
+
+        System.out.println("Exiba a posição da nota 5.0: " + notas.indexOf(5D));
+
+
+        System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
+
+        notas.add(4, 8.0D);
+
+        System.out.println(notas);
+
+        System.out.println("Substitua a nota 5.0 pela nota 6.0: ");
+
+        notas.set(notas.indexOf(5D), 6.0);
+
+        System.out.println(notas.toString());
+
+        System.out.println("Confira se a nota 5.0 está na lista: " + notas.contains(5.0D));
+
+        System.out.println("Exiba todas as notas na ordem em que foram informados: ");
+
+        for (Double nota: notas) System.out.println(nota);
+
 //        System.out.println("Exiba a terceira nota adicionada");
 //
 //        System.out.println("Exiba a menor nota: ");
