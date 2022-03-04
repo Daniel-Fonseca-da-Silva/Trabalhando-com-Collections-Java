bc765676473ae6d270818f14578ff3e3daa75102
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-03-04 16:38:50 -0300

    Conhecendo os métodos List - parte 2

--- src/com/dio/list/ExemploList.java
+++ src/com/dio/list/ExemploList.java
@@ -52,26 +52,54 @@ class ExemploList {
 
         for (Double nota: notas) System.out.println(nota);
 
-//        System.out.println("Exiba a terceira nota adicionada");
-//
-//        System.out.println("Exiba a menor nota: ");
-//
-//        System.out.println("Exiba a maior nota: ");
-//
-//
-//        System.out.println("Exiba a soma dos valores: ");
-//
-//        System.out.println("Exiba a média das notas: ");
-//
-//        System.out.println("Remova a nota 0: ");
-//
-//        System.out.println("Remova a nota da posição 0");
-//
-//        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+        System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
 
-//        System.out.println("Apague toda a lista");
-//
-//        System.out.println("Confira se a lista está vazia: ");
+        System.out.println(notas);
+
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));
+
+        Iterator<Double> iterator = notas.iterator();
+
+        Double soma = 0D;
+
+        while(iterator.hasNext()) {
+            Double next = iterator.next();
+            soma += next;
+        }
+
+        System.out.println("Exiba a soma dos valores: " + soma);
+
+        System.out.println("Exiba a média das notas: " + (soma/notas.size()));
+
+        System.out.println("Remova a nota 0: " + notas.remove(0.0D));
+
+        System.out.println(notas.toString());
+
+        System.out.println("Remova a nota da posição 0");
+        notas.remove(0);
+
+        System.out.println(notas);
+
+        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+
+        Iterator<Double> iterator1 = notas.iterator();
+
+        while(iterator1.hasNext()) {
+            Double next = iterator1.next();
+            if(next < 7) iterator1.remove();
+        }
+
+        System.out.println(notas);
+
+        System.out.println("Apague toda a lista");
+
+        notas.clear();
+
+        System.out.println("Confira se a lista está vazia: ");
+
+        System.out.println(notas.isEmpty());
 
         /*
         Para você: Resolva esses exercícios utilizando os métodos da implementação LinkedList:
