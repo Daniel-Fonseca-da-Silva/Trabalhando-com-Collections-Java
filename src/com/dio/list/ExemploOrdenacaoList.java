1055c4592fafc1e8925a3b20b0ca5f5efb61ddec
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-03-06 21:12:44 -0300

    Adicionado arquivo ListOdernação

---
+++ src/com/dio/list/ExemploOrdenacaoList.java
@@ -0,0 +1,27 @@
+package com.dio.list;
+
+/*Dadas as seguintes informações sobre meus gatos, crie uma lista
+e ordene esta lista exibindo:
+(nome - idade - cor);
+
+Gato 1 = nome: Jon, idade: 18, cor: preto
+Gato 2 = nome: Simba, idade: 6, cor: tigrado
+Gato 3 = nome: Jon, idade: 12, cor: amarelo
+*/
+
+public class ExemploOrdenacaoList {
+    public static void main(String[] args) {
+
+//        System.out.println("--\tOrdem de Inserção\t---");
+//
+//        System.out.println("--\tOrdem aleatória\t---");
+//
+//        System.out.println("--\tOrdem Natural (Nome)\t---");
+//
+//        System.out.println("--\tOrdem Idade\t---");
+//
+//        System.out.println("--\tOrdem cor\t---");
+//
+//        System.out.println("--\tOrdem Nome/Cor/Idade\t---");
+//    }
+//}
