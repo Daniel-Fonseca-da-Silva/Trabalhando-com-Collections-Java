1e9554fc01d0a370a26a54a2451c7cbe8d4c8e25
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-03-11 14:57:10 -0300

    Adicionado arquivo ExemploSet

---
+++ src/com/dio/set/ExemploSet.java
@@ -0,0 +1,55 @@
+package com.dio.set;
+
+import java.util.*;
+
+public class ExemploSet {
+    public static void main(String[] args) {
+// Dada uma lista com 7 notas de um aluno [7, 8.5, 9.3, 5, 7, 0, 3.6], faça:
+
+//      Set notas = new HashSet(); //antes do java 5
+//      HashSet<Double> notas = new HashSet<>();
+//      Set<Double> notas = new HashSet<>(); //Generics(jdk 5) - Diamont Operator(jdk 7)
+/*      Set<Double> notas = Set.of(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
+        notas.add(1d);
+        notas.remove(5d);
+        System.out.println(notas);
+*/
+
+//        System.out.println("Crie um conjunto e adicione as notas: ");
+
+//        System.out.println("Exiba a posição da nota 5.0: ");
+
+//        System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
+
+//        System.out.println("Substitua a nota 5.0 pela nota 6.0: ");
+
+//        System.out.println("Confira se a nota 5.0 está no conjunto: ");
+
+//        System.out.println("Exiba a terceira nota adicionada: ");
+
+//        System.out.println("Exiba a menor nota: ");
+
+//        System.out.println("Exiba a maior nota: ");
+
+//        System.out.println("Exiba a soma dos valores: ");
+
+//        System.out.println("Exiba a média das notas: ");
+
+//        System.out.println("Remova a nota 0: ");
+
+//        System.out.println("Remova a nota da posição 0");
+
+//        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+
+//        System.out.println("Exiba todas as notas na ordem em que foram informados: ");
+
+//        System.out.println("Exiba todas as notas na ordem crescente: ");
+
+//        System.out.println("Apague todo o conjunto");
+
+//        System.out.println("Confira se o conjunto está vazio: ");
+//        System.out.println("Confira se o conjunto 2 está vazio: ");
+//        System.out.println("Confira se o conjunto 3 está vazio: ");
+
+    }
+}
